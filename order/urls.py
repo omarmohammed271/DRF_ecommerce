4d8c0105e9a487508cd1e9528ba03ce757d35d5f
moooooora <omarmohammed271@gmail.com>
2024-04-15 12:22:55 +0200

    Added Viewsets to Order and OrderProduct

--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from . import views
 
 
 app_name = 'order'
 
+router = DefaultRouter()
+router.register('order',views.OrderView)
+router.register('orderproduct',views.OrderProductView)
+
 urlpatterns = [
     
 ]
+urlpatterns += router.urls
 
--- order/views.py
+++ order/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets,status
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .models import Order,OrderProduct
+from .serializers import OrderProductSerializer,OrderSerializer
 # Create your views here.
+
+class OrderView(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+
+class OrderProductView(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = OrderProduct.objects.all()
+    serializer_class = OrderProductSerializer
+
+
